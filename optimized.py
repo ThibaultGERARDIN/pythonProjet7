c3e704944c48e2f6f393f277756c98d3078c0b7f
Thibault G <thibault.gerardin@gmail.com>
2024-11-29 15:47:18 +0100

    modification commentaires

--- optimized.py
+++ optimized.py
@@ -2,6 +2,7 @@ import csv
 
 
 def get_data():
+    # Read CSV file and return usable data : action = (name, cost, gain)
     actions = []
     with open("actions.csv", mode="r") as file:
         reader = csv.DictReader(file)
@@ -15,50 +16,50 @@ def get_data():
     return actions
 
 
-def sac_a_dos(actions, max_budget):
+def get_best_combination(actions, max_budget):
     n = len(actions)
-    # Créer une matrice de taille (n+1) x (budget_max+1)
+    # Create a matrix of size (n+1)(max_budget +1) and initialize it at 0
     matrix = [[0 for i in range(max_budget + 1)] for y in range(n + 1)]
 
-    # Remplir la matrice
+    # Fill the matrix while choosing which action is bought
     for i in range(1, n + 1):
         name, cost, gain = actions[i - 1]
         for b in range(max_budget + 1):
             if cost <= b:
-                # On choisit entre prendre l'action ou non
+                # Either buy or not the action (maximum gain of the two)
                 matrix[i][b] = max(
                     matrix[i - 1][b], matrix[i - 1][b - cost] + gain
                 )
             else:
-                # On ne peut pas prendre l'action
+                # Can't buy the action
                 matrix[i][b] = matrix[i - 1][b]
 
-    # Retrouver la combinaison optimale
+    # Read the matrix in reverse to obtain the combination
     combination = []
     b = max_budget
     for i in range(n, 0, -1):
         if matrix[i][b] != matrix[i - 1][b]:
-            combination.append(actions[i - 1])  # Ajouter l'action
-            b -= actions[i - 1][1]  # Réduire le budget disponible
+            combination.append(actions[i - 1])
+            b -= actions[i - 1][1]
 
-    best_gain = matrix[n][max_budget]
+    best_gain = round(matrix[n][max_budget], 2)
     return combination, best_gain
 
 
-# Fonction principale
 def main():
-    max_budget = 700
+    max_budget = 500
 
     actions = get_data()
-    best_combination, best_gain = sac_a_dos(actions, max_budget)
+    best_combination, best_gain = get_best_combination(actions, max_budget)
 
     print("Meilleure combinaison d'actions :")
     for action in best_combination:
         print(
-            f"- {action[0]} (Coût: {action[1]} euros, Bénéfice: {action[2]:.2f} euros)"
+            f"- {action[0]} (Coût: {action[1]} euros,"
+            f" Bénéfice: {action[2]} euros)"
         )
 
-    print(f"\nBénéfice total : {best_gain:.2f} euros")
+    print(f"\nBénéfice total : {best_gain} euros")
 
 
 if __name__ == "__main__":
